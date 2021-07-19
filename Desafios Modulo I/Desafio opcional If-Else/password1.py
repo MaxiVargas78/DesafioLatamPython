2df3ffcd247dfcc7b4cd38af8db7d7db6f988385
MaxiVargas78 <maxx7689@gmail.com>
2021-07-19 02:55:37 -0400

    Desafio Opcional

---
+++ Desafios Modulo I/Desafio opcional If-Else/password1.py
@@ -0,0 +1,9 @@
+
+password = input("Ingrese una clave de al menos seis digitos:")
+if(len(password) < 6):
+    if (password == "12345"):
+        print("Su clave es incorrecta")
+    else:
+        print("Su clave debe ser mayor a seis digitos")
+else:
+    pass
