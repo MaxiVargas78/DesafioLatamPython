560abb1d28fae05f794705525af0cf7f503af484
MaxiVargas78 <maxx7689@gmail.com>
2021-08-09 21:24:23 -0400

    Desafios Modulo III + Opcionales completos

--- Desafios Modulo III/Desafio I/asociar.py
+++ Desafios Modulo III/Desafio I/asociar.py
@@ -1,12 +1,5 @@
 from velocidad import *
 
-velocidad = [4, 4, 7, 7, 8, 9, 10, 10, 10,
- 11, 11, 12, 12, 12, 12, 13, 13,
- 13, 13, 14, 14, 14, 14, 15, 15,
- 15, 16, 16, 17, 17, 17, 18, 18,
- 18, 18, 19, 19, 19, 20, 20, 20,
- 20, 20, 22, 23, 24, 24, 24, 24, 25]
-
 distancia = [2, 10, 4, 22, 16, 10, 18, 26, 34,
  17, 28, 14, 20, 24, 28, 26, 34, 34,
  46, 26, 36, 60, 80, 20, 26, 54, 32,
@@ -21,16 +14,43 @@ for lista in zip(velocidad, distancia):
 matrix1 = []
 for j in matrix:
     matrix1.append(j[0])
+
+matrix2 = []
+for k in matrix:
+    matrix2.append(k[1])
+
 a = promedio(matrix1)
+b = promedio(matrix2)
 
 count = 0
 for i in range(len(matrix1)):
-    if (matrix1[i] > a):
+    if (matrix1[i] < a):
         count += 1
     else:
         None
 
-print("Contador 1: "+str(count))
+count2 = 0
+for i in range(len(matrix1)):
+    if (matrix1[i] < a and matrix2[i] > b):
+        count2 += 1
+    else:
+        None
 
+count3 = 0
+for i in range(len(matrix1)):
+    if (matrix1[i] > a):
+        count3 += 1
+    else:
+        None
 
+count4 = 0
+for i in range(len(matrix1)):
+    if (matrix1[i] > a and matrix2[i] < b):
+        count4 += 1
+    else:
+        None
 
+print("Velocidad bajo el promedio: "+str(count))
+print("Velocidad bajo el promedio y distancia sobre el promedio: "+str(count2))
+print("Velocidad sobre el promedio: "+str(count3))
+print("Velocidad sobre el promedio y distancia bajo el promedio: "+str(count4))
--- Desafios Modulo III/Opcional - OlimpiadasV2/olimpiadasv2.py
+++ Desafios Modulo III/Opcional - OlimpiadasV2/olimpiadasv2.py
@@ -1,5 +1,84 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('athlete_events.csv')
 
-subset = df.loc[:, ["Name","Team", "Year", "Medal"]]
+subset = df.loc[:, ["Name","Team", "Year", "Medal", "Sex"]].fillna(0)
+
+#EJERCICIO I
+subset2 = subset[subset["Medal"] == "Gold" ].loc[:, ["Name", "Medal", "Team", "Sex"]] # Todos los ganadores de Oro
+subset3 = subset[subset["Medal"] == "Silver" ].loc[:, ["Name", "Medal", "Team", "Sex"]] # Todos los ganadores de Plata
+subset4 = subset[subset["Medal"] == "Bronze" ].loc[:, ["Name", "Medal", "Team", "Sex"]] # Todos los ganadores de Bronce
+subset5 = subset[subset["Medal"] == 0 ].loc[:, ["Name", "Medal", "Team", "Sex"]] # Atletas sin medallas
+
+#EJERCICIO II
+subset['Female'] = ["1" if s == "F" else "0" for s in subset['Sex']]
+#print(subset) #descomentar para visualizar tabla con nueva columna 'Female'
+
+#EJERCICIO III
+#Mayor cantidad Gold
+subset6 = subset2["Team"].value_counts().head(10)
+print("#Mayor cantidad Gold:")
+print(subset6)
+print("\n")
+
+#Mayor cantidad Silver
+subset7 = subset3["Team"].value_counts().head(10)
+print("#Mayor cantidad Silver:")
+print(subset7)
+print("\n")
+
+#Mayor cantidad Bronze
+subset8 = subset4["Team"].value_counts().head(10)
+print("#Mayor cantidad Bronze:")
+print(subset8)
+print("\n")
+
+#Mayor cantidad SinMedalla
+subset9 = subset5["Team"].value_counts().head(10)
+print("#Mayor cantidad SinMedalla:")
+print(subset9)
+print("\n")
+
+#EJERCICIO IV
+#Cantidad x genero Gold
+subset10 = subset2["Sex"].value_counts().head(10)
+print("#Cantidad x genero Gold:")
+print(subset10)
+print("\n")
+
+#Cantidad x genero Silver
+subset11 = subset3["Sex"].value_counts().head(10)
+print("#Cantidad x genero Silver:")
+print(subset11)
+print("\n")
+
+#Cantidad x genero Bronze
+subset12 = subset4["Sex"].value_counts().head(10)
+print("#Cantidad x genero Bronze:")
+print(subset12)
+print("\n")
+
+#Cantidad x genero SinMedalla
+subset13 = subset5["Sex"].value_counts().head(10)
+print("#Cantidad x genero SinMedalla:")
+print(subset13)
+print("\n")
+
+#EJERCICIO V
+def media(df, analyze, gender):
+    subset14 = df.loc[:, [analyze, "Sex"]]
+    subset15 = subset14[subset14["Sex"] == gender].value_counts().mean()
+    subset16 = subset14[subset14["Sex"] != gender].value_counts().mean()
+    media = (int(subset15) + int(subset16))/2
+    print("La media entre Hombres y mujeres en "+str(analyze)+" es "+str(media))
+
+media(subset,"Team", "F") #rellenar los campos para saber la media en cada columna
+#Para el item 6.1 cambiar el subset por cada subset creado anteriormente
+
+#EJERCICIO VI
+subset17 = df.loc[:, ["Height", "Weight"]]
+subset18 = subset17["Height"].mean()
+subset19 = subset17["Weight"].mean()
+print("La media de columna Height es: "+str(subset18))
+print("La media de columna Weight es: "+str(subset19))
