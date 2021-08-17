f775000dc59e071cb008f39f2ae9adacbd2a62f1
MaxiVargas78 <maxx7689@gmail.com>
2021-08-16 23:53:58 -0400

    Examen final

--- Prueba Final/PruebaFinal.py
+++ Prueba Final/PruebaFinal.py
@@ -1,10 +1,7 @@
-#API KEY NASA: PcSJslTQHn98bupJkZWlmdRRPKUIw2mz7gcNvtnp
 import requests
 import json
 
 url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=PcSJslTQHn98bupJkZWlmdRRPKUIw2mz7gcNvtnp"
-
-
 payload = {}
 headers = {}
 response = requests.request("GET", url,headers=headers, data=payload)
@@ -12,6 +9,25 @@ response = json.loads(response.text)
 
 listaFotos = response["latest_photos"]
 
-for i in listaFotos:
-    listaFotos[i]
-    print(listaFotos)
+# REQUERIMIENTO NUMERO UNO
+UpdateFoto = []
+for i in range(25):
+    UpdateFoto.append(listaFotos[i])
+
+# REQUERIMIENTO NUMERO DOS
+UrlFotos = []
+for j in UpdateFoto:
+    UrlFotos.append(j["img_src"])
+
+# REQUERIMIENTO NUMERO TRES
+def build_web_page(UrlFotos):
+    html = ""
+    for k in UrlFotos:
+        html += "<li><img src=\"{}\"></li>\n".format(k)
+
+    with open("HTML_NASA.html", "w") as f:
+        f.write(" <html> \n <head> \n </head> \n <body> \n <ul>")
+        f.write(html)
+        f.write(" </ul> \n </body> \n </head> \n </html>")
+
+build_web_page(UrlFotos)
